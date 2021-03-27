ef4bcc7894b1bfcda33f81251afa822ec277a502
anne <amlovell2@outlook.com>
2021-03-27 06:07:34 -0400

    Plea enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace deliverable_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+        Restart:
+            Console.Write("Please enter a measurement type: ");
+            string userInput = Console.ReadLine();
+
+            Console.Write("Please enter a numerical amount: ");
+            string userInput2 = Console.ReadLine();
+            double amount = double.Parse(userInput2);
+
+            if (userInput.ToLower() == "inch")
+            {
+                double inchConversion = 3.5 * amount;
+
+                Console.WriteLine($" { userInput2 } inch(es) is equivalent to { inchConversion } fidget spinners.");
+                //Console.Write("This is" + inchConversion.ToString() + " in fitdget spinners");
+                //Console.Write("This is " + inchConversion + " in fidget spinners");
+                //Console.Write($"This is { 3.5 * amount } in fidget spinners");
+            }
+            else if (userInput.ToLower() == "fidget spinners")
+            {
+                double fidgetSpinnersConversion = amount / 3.5;
+
+                Console.WriteLine($" { userInput2 } fidget spinner(s) is equivalent to { fidgetSpinnersConversion } inches.");
+            }
+            else if (userInput.ToLower() == "feet")
+            {
+                double feetConversion = 5 * amount;
+
+                Console.WriteLine($" { userInput2 } feet is equivalent to { feetConversion } memes.");
+            }
+            else if (userInput.ToLower() == "memes")
+            {
+                double memesConversion = amount / 5;
+
+                Console.WriteLine($"{ userInput2 } meme(s) is equivalent to { memesConversion } feet.");
+
+            }
+
+        
+            {
+                Console.WriteLine("Would you like to do another calculation? (Y/N) ");
+                string userInput3 = Console.ReadLine();
+
+             while (userInput3.ToLower() == "y")
+                {
+                    Console.WriteLine("Awesome!");
+                    goto Restart;
+                }
+              while (userInput3.ToLower() == "n")
+               {
+                    Console.Write("Thank you!");
+                    break;
+               }
+            }
+
+            
+
+            
+        }
+    }
+}
